07b3e127491174c15ceb487dad275862a165d449
Gustavo de Oliveira Feitosa <gof@ug2c18.windows.cin.ufpe.br>
2018-04-27 08:11:55 -0300

    inicializacao dos clusters e correcoes na linha 18 e 24

--- cluster.py
+++ cluster.py
@@ -11,16 +11,18 @@ class Partition:
 
 		self.initialize_parameters()
 
-	def initialize_parameters(self):
-		gamma = 1
-		
+	def initialize_parameters(self):		
 		#Number of variables
 		p = len(self.view.columns)
+
+		gamma = 1
+		# gamma = (1/sigma)**p
 		#Global weights for each variable
 		self.weights = p*[None]
 		#Initialize weights
 		for i in range(0, p):
 			self.weights[i] = gamma**(1/p)
+			# self.weights[i] = 1/sqrt(gamma**(1/p))
 
 		#Set initial random prototypes
 		self.prototypes = self.view.sample(frac=1)[0:7].index.values
@@ -39,6 +41,7 @@ def gaussian_kernel(global_weights, x, y):
 	argument = 0
 	for p_j, weight in enumerate(global_weights):
 		argument += weight*(x[p_j] - y[p_j])**2
+		# argument += (1/(weight**2))*((x[p_j] - y[p_j])**2)
 
 	return np.exp((-1/2)*argument)
 
@@ -51,3 +54,17 @@ groups_class = data["CLASS"]
 part = Partition(7, view)
 part.print_prot()
 part.print_weights()
+
+def clustering(view, qtd_cluster, global_weights, prototypes):
+	cluster = np.empty([qtd_cluster, len(view)])
+
+	for i in range (0, len(view)):
+		menor_cluster = infinito
+		object_i = 0
+		for j in range (0, qtd_cluster):
+			x = 2*(1 - gaussian_kernel(global_weights, view.iloc[i], prototypes[j]))
+			if (x < menor_cluster):
+				menor_cluster = x
+				object_i = j
+
+		cluster[object_i].append = i
