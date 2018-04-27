3e95fab20c708d4b36e4dc02ba33dfe9e30811f5
Andrea Brandao Duque <abd@ug1c18.windows.cin.ufpe.br>
2018-04-26 21:51:27 -0300

    Partition class with gaussian kernel and initialization

--- cluster.py
+++ cluster.py
@@ -1,25 +1,53 @@
 import pandas as pd
-import numpy
+import numpy as np
 from sklearn.metrics.pairwise import euclidean_distances
 
 data = pd.read_csv("segmentation.csv", sep=",")
 
+class Partition:
+	def __init__(self, nclusters, view):
+		self.c = nclusters
+		self.view = view
 
-def euclidean_matrix(view):
+		self.initialize_parameters()
 
+	def initialize_parameters(self):
+		gamma = 1
+		
+		#Number of variables
+		p = len(self.view.columns)
+		#Global weights for each variable
+		self.weights = p*[None]
+		#Initialize weights
+		for i in range(0, p):
+			self.weights[i] = gamma**(1/p)
 
-def get_prototypes(nprototypes, view):
-	gamma = 1
+		#Set initial random prototypes
+		self.prototypes = self.view.sample(frac=1)[0:7].index.values
 
-	print(view.sample(frac=1)[0:7])
+	def set_partitions(self, weights):
+		pass
 
+	def print_prot(self):
+		print(self.prototypes)
 
-def set_initial_parameters():
+	def print_weights(self):
+		print(self.weights)
+
+def gaussian_kernel(global_weights, x, y):
+
+	argument = 0
+	for p_j, weight in enumerate(global_weights):
+		argument += weight*(x[p_j] - y[p_j])**2
+
+	return np.exp((-1/2)*argument)
+
+# print(gaussian_kernel(global_weights, np.array(view.iloc[0]), np.array(view.iloc[1])))
 
 
 view = data.drop(axis=1, columns = ["CLASS"])
 groups_class = data["CLASS"]
-#get_prototypes(7, data)
 
-#primeiro elemento
-print(np.array(view.iloc[0]))
+part = Partition(7, view)
+part.print_prot()
+part.print_weights()
