17453f8caac7465064d40d56e1d2027f2779368f
Andrea Brandao Duque <abd@ug5c25.windows.cin.ufpe.br>
2018-04-27 11:16:05 -0300

    initialize partitions

--- cluster.py
+++ cluster.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import euclidean_distances
+import functions as f
 
 data = pd.read_csv("segmentation.csv", sep=",")
 
@@ -10,25 +11,43 @@ class Partition:
 		self.view = view
 
 		self.initialize_parameters()
+		self.initialize_partitions()
+
 
 	def initialize_parameters(self):		
 		#Number of variables
 		p = len(self.view.columns)
-
-		gamma = 1
-		# gamma = (1/sigma)**p
+		#A suitable parameter
+		gamma = f.gamma(self.view.as_matrix())
 		#Global weights for each variable
 		self.weights = p*[None]
 		#Initialize weights
 		for i in range(0, p):
 			self.weights[i] = gamma**(1/p)
-			# self.weights[i] = 1/sqrt(gamma**(1/p))
 
 		#Set initial random prototypes
 		self.prototypes = self.view.sample(frac=1)[0:7].index.values
 
-	def set_partitions(self, weights):
-		pass
+
+	def initialize_partitions(self):
+		self.elements = {}
+
+		el_not_prot = list(set(range(0, len(view))) - set(self.prototypes))
+
+		for el in el_not_prot:
+			dist = float("inf")
+			nearest_cluster = 0
+			
+			for h in range(0, self.c):
+				#Kernel between element and cluster prototype
+				x = 2*(1 - f.gaussian_kernel(self.weights, np.array(self.view.iloc[h]),
+					np.array(self.view.iloc[self.prototypes[h]])))
+
+				if (x < dist):
+					nearest_cluster = h
+					dist = x
+			
+			self.elements[el] = nearest_cluster
 
 	def print_prot(self):
 		print(self.prototypes)
@@ -36,16 +55,11 @@ class Partition:
 	def print_weights(self):
 		print(self.weights)
 
-def gaussian_kernel(global_weights, x, y):
+	def print_elements(self):
+		print(self.elements)
 
-	argument = 0
-	for p_j, weight in enumerate(global_weights):
-		argument += weight*(x[p_j] - y[p_j])**2
-		# argument += (1/(weight**2))*((x[p_j] - y[p_j])**2)
 
-	return np.exp((-1/2)*argument)
-
-# print(gaussian_kernel(global_weights, np.array(view.iloc[0]), np.array(view.iloc[1])))
+# print(f.gaussian_kernel(global_weights, np.array(view.iloc[0]), np.array(view.iloc[1])))
 
 
 view = data.drop(axis=1, columns = ["CLASS"])
@@ -54,17 +68,4 @@ groups_class = data["CLASS"]
 part = Partition(7, view)
 part.print_prot()
 part.print_weights()
-
-def clustering(view, qtd_cluster, global_weights, prototypes):
-	cluster = np.empty([qtd_cluster, len(view)])
-
-	for i in range (0, len(view)):
-		menor_cluster = infinito
-		object_i = 0
-		for j in range (0, qtd_cluster):
-			x = 2*(1 - gaussian_kernel(global_weights, view.iloc[i], prototypes[j]))
-			if (x < menor_cluster):
-				menor_cluster = x
-				object_i = j
-
-		cluster[object_i].append = i
+part.print_elements()
