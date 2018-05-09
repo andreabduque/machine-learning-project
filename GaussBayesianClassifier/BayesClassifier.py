5249dbc5ab2d6fa3ea3a23b3c8b2039407d19ed5
jvcm <camposmoraes@outlook.com>
2018-05-09 10:27:04 -0300

    Kfold atualizado

---
+++ GaussBayesianClassifier/BayesClassifier.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+from math import exp
+import random
+from sklearn.model_selection import StratifiedKFold
+
+class BayesClassifier:
+    
+    def __init__(self):
+        self.covariance = None
+        self.mean = None
+        self.N = 0
+        self.classes = None
+        return
+        
+    def parameters(self,data): #Entra DataFrame
+        self.N = len(data)
+        self.classes = data["CLASS"].value_counts().to_dict()
+        data_x = data.drop(axis=1, columns = ["CLASS"])
+        d = len(data_x.iloc[0])
+        self.covariance = np.array(data_x.cov())
+        #SHIT
+        for i in range(d):
+            for j in range(d):
+                if i!=j:
+                    self.covariance[i,j]=0
+        #SHIT
+        mean = np.zeros((len(self.classes),d))
+        for i,classe in enumerate(self.classes):
+            mean[i] = np.array(data.loc[data["CLASS"]==classe].mean())
+        self.mean = mean
+        return
+    
+    def classify(self, x): #Entra np.array
+        inv_covar = np.linalg.inv(self.covariance)
+        g_x = np.zeros(len(self.classes))
+        p_c = np.zeros(len(self.classes))
+        for i,classe in enumerate(self.classes):
+            p_c[i] = self.classes[classe]/self.N
+            g_x[i] = (-0.5*(np.matmul(np.matmul((x-self.mean[i]),inv_covar),(x-self.mean[i])))) + np.log(p_c[i]) 
+        return(list(self.classes.keys())[np.argmax(g_x)])
+    
+    def accuracy(self,Test): #Entra DataFrame
+        coef = 0
+        data_x = Test.drop(axis=1, columns = ["CLASS"])
+        for i in Test.index.tolist():
+            if self.classify(np.array(data_x.loc[i])) == Test.loc[i,"CLASS"]:
+                coef += 1
+        return coef/len(Test)
+    
+    def KfoldNtimes(self,k,n,data):
+        X = np.array(data.drop(axis=1, columns = ["CLASS"]))
+        y = np.array(data["CLASS"])
+        skf = StratifiedKFold(n_splits=k,shuffle=True)
+        skf.get_n_splits(X, y)
+        l = []
+        for i in range(n):
+            media = 0
+            for train_index, test_index in skf.split(X, y):
+                #print("TRAIN:", train_index, "TEST:", test_index)
+                self.parameters(df.loc[train_index])
+                media += self.accuracy(df.loc[test_index])
+            l.append(media/10)
+        return(l)
+
+#TESTE
+df = pd.read_csv('segmentation1.csv')
+modelo = BayesClassifier()
+print(modelo.KfoldNtimes(10,30,df))
