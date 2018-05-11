83242b095653a09132db88461d7b31ec221065b7
gustavo.oliveira <gustavo.oliveira@consensotec.com.br>
2018-05-11 20:59:14 -0300

    estou com pressa

--- parzen/parzen.py
+++ parzen/parzen.py
@@ -7,7 +7,8 @@ import pdb
 class Parzen:
 
     def __init__(self, data):
-        self.h = self.init_h(data)[4] # metade do array
+        # self.h = self.init_h(data)[4] # metade do array
+        self.h = 0.2
         self.data = data
         self.n = data.shape[0]
         self.p = data.shape[1]
@@ -21,16 +22,29 @@ class Parzen:
         return [0.04*int(10*random.random()) for i in range(10)] # estudarcomo melhorara a convergencia ~ em função do conjunto de validação
 
     def parzen(self, data, x):
-        sum = self.n * [None]
-        for i in range(self.n):
-            prod = self.p * [None]
-            for j in range(self.p):
+        sum = data.shape[0] * [None]
+        for i in range(data.shape[0]):
+            prod = data.shape[1] * [None]
+            for j in range(data.shape[1]):
                 prod[j] = self.kernel((x[j] - data[i][j])/self.h)
             sum[i] = np.prod(prod)
         
         return (1/(self.n*(self.h**7)))*np.sum(sum)
 
+    def prob(self, data, x):
+        self.classes = data["CLASS"].value_counts().to_dict()
+        data_x = data.drop(axis=1, columns = ["CLASS"])
+
+        p_w_x = len(self.classes) * [None]
+        for i, classe in enumerate(self.classes):
+            # pdb.set_trace()
+            classe = np.array(data_x.loc[data["CLASS"]==classe])
+
+            p_w_x[i] = self.parzen(classe, x)
+
+        return np.argmax(p_w_x)
+
 #TESTE
 df = pd.read_csv('segmentation1.csv')
 modelo = Parzen(np.array(df.drop(axis=1, columns = ["CLASS"])))
-print(modelo.parzen(modelo.data, modelo.data[0])) # teste zoado
+print(modelo.prob(df, modelo.data[0])) # teste zoado
